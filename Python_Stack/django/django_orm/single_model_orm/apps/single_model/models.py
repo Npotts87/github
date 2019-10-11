01267353203b8a33df849e39153c61e41050dc64
Npotts87 <nathanrpotts@gmail.com>
2019-10-10 18:37:25 -0700

    daily_update

--- Python_Stack/django/django_orm/single_model_orm/apps/single_model/models.py
+++ Python_Stack/django/django_orm/single_model_orm/apps/single_model/models.py
@@ -3,7 +3,10 @@ from django.db import models
 class User(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
-    eamil_address = models.CharField(max_length=255)
+    email_address = models.CharField(max_length=255)
     age = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    
+    def __repr__(self):
+        return f"User: {self.first_name} {self.last_name}, age: {self.age}"
