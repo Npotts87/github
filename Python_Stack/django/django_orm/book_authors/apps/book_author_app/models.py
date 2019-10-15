5b741e8893c234c59f84eee601a4ab146c0dd474
Npotts87 <nathanrpotts@gmail.com>
2019-10-15 08:58:29 -0700

    daily_update

--- Python_Stack/django/django_orm/book_authors/apps/book_author_app/models.py
+++ Python_Stack/django/django_orm/book_authors/apps/book_author_app/models.py
@@ -1,23 +1,22 @@
 from django.db import models
 
-class Dojo(models.Model):
-    name = models.CharField(max_length=255)
-    city = models.CharField(max_length=255)
-    state = models.CharField(max_length=2)
+class Book(models.Model):
+    title = models.CharField(max_length=255)
     desc = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __repr__(self):
-        return f"Dojo: {self.name} - {self.city}, {self.state}."
+        return f"Books: {self.title} {self.id}"
 
 
-class Ninja(models.Model):
-    dojo = models.ForeignKey(Dojo, related_name="ninjas")
+class Author(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
+    notes = models.CharField(max_length=255)
+    books = models.ManyToManyField(Book, related_name="authors")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __repr__(self):
-        return f"Ninja: {self.first_name} {self.last_name}"
+        return f"Author: {self.first_name} {self.last_name} - "
