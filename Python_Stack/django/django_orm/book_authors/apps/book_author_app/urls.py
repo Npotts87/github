a85c955f149ca80a97aea197e7efff6130a3cef8
Npotts87 <nathanrpotts@gmail.com>
2019-10-16 12:58:43 -0700

    daily_update

--- Python_Stack/django/django_orm/book_authors/apps/book_author_app/urls.py
+++ Python_Stack/django/django_orm/book_authors/apps/book_author_app/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
-from . import views	# the . indicates that the views file can be found in the same directory as this file
+from . import views	
                     
 urlpatterns = [
     url(r'^$', views.index),
     url(r'^add_new_book$', views.add_new_book),
-    url(r'^view_book$', views.view_book),
+    url(r'^create_book$', views.create_book),    
+    url(r'^view_book/(?P<book_id>\d+)$', views.view_book),
     url(r'^add_new_author$', views.add_new_author),
-    url(r'^view_author$', views.view_author),
+    url(r'^create_author$', views.create_author,    
+    url(r'^view_author/(?P<author_id>\d+)$', views.view_author),
 ]
--- Python_Stack/django/django_orm/book_authors/apps/book_author_app/views.py
+++ Python_Stack/django/django_orm/book_authors/apps/book_author_app/views.py
@@ -1,22 +1,28 @@
 from django.shortcuts import render, redirect
-from .models import Author
+from .models import Author, Book
 
 def index(request):
-    context = {"authors": Author.objects.all()}
+    context = {"home": Author.objects.all()}
     return render(request, "book_author_app/index_books_authors.html", context)
 
-def view_book(request):
-    context = {"authors": Author.objects.all()}
-    return render(request, "book_author_app/index_view_book.html", context)
-
 def add_new_book(request):
-    context = {"authors": Author.objects.all()}
+    context = {"books": Book.objects.all()}
     return render(request, "book_author_app/index_add_book.html", context)
 
-def view_author(request):
-    context = {"authors": Author.objects.all()}
-    return render(request, "book_author_app/index_view_author.html", context)
+def create_book(request):
+    if request.method == "POST":
+        newly_created_book = Book.objects.create(title=request.POST['title'],desc=request.POST['desc'])
+        newly_created_book.save()
+        return redirect("/view_book/"+str(newly_created_book.id))
+
+def view_book(request, book_id):
+    context = {"book": Book.objects.get(id=book_id)}
+    return render(request, "book_author_app/index_view_book.html", context)
 
 def add_new_author(request):
     context = {"authors": Author.objects.all()}
     return render(request, "book_author_app/index_add_author.html", context)
+
+def view_author(request):
+    context = {"authors": Author.objects.all()}
+    return render(request, "book_author_app/index_view_author.html", context)
